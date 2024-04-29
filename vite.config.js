d316fa566d959db161e0aa0b88ca466a0727421a
Pulkit saini <51844439+sainipulkit12@users.noreply.github.com>
2024-04-29 21:12:54 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/E-comm-react-redux-firebase",
+  base: "/E-comm-react-redux-firebase/",
   plugins: [react()],
 })
