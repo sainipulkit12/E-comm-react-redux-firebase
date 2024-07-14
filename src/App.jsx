e8e4cc5885f06b080f62828188c5f02292b3aaad
Pulkit saini <51844439+sainipulkit12@users.noreply.github.com>
2024-07-15 00:45:05 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Children } from 'react'
-import { BrowserRouter as Router, Route, Routes, useNavigate, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, useNavigate, Navigate } from "react-router-dom";
 import Home from './pages/home/Home';
 import Order from './pages/order/Order';
 import Cart from './pages/cart/Cart';
@@ -51,7 +51,7 @@ function App() {
               <UpdateProduct />
             </ProtectedRouteForAdmin>
           } />
-          <Route path='*' element={<Nopage />} />
+          <Route path='/*' element={<Nopage />} />
         </Routes>
         <ToastContainer />
       </Router>
